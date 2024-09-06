763011ed44ec5f7dc965b1c8825ebfe9c9fee7d3
4hmadanbiy4 <4hmadanbiy4@gmail.com>
2024-09-06 15:11:57 +0700

    update setting.py

--- manhaj_books/settings.py
+++ manhaj_books/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s47ic&&2tyfgbw09@f8zhnisak+50=s)98w3=k8!)zg^q(j^=6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahmad-nizar31-manhajbooks.pbp.cs.ui.ac.id"]
 
 
 # Application definition
