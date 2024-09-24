b21ecf71614c1fcd648a2624e34242cd77901a0a
4hmadanbiy4 <4hmadanbiy4@gmail.com>
2024-09-24 13:42:07 +0700

    Tugas 4 - implementasi function regis, login, dan logout

--- main/views.py
+++ main/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.core import serializers
 from .models import Product, Reservation
 from .forms import ReservationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def show_main(request):
@@ -110,6 +114,35 @@ def create_reservation_entry(request):
         }
     return render(request, "create_reservation_entry.html", context)
 
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('main:login')
 
 def show_json(request):
     data = Reservation.objects.all()
